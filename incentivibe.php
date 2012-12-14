4fbfc7fdc51094092fb596d7feca3a66d77cf980
amunda <amunda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-14 16:56:02 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/incentivibe/trunk@639293 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ incentivibe.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Plugin Name: Incentivibe
+Plugin URI: http://www.incentivibe.com
+Description: Increase email leads and social media exposure by up to 400%!
+Version: 1.0.1
+Author: Incentivibe
+Author URI: www.incentivibe.com
+*/
+
+define('INCENTIVIBE_VERSION', '1.0.1');
+
+add_action('admin_menu', 'incentivibe_admin_menu');
+add_action('wp_head', 'incentivibe_wp_head');
+
+function incentivibe_admin_menu(){
+	add_menu_page( __('Incentivibe'), __('Incentivibe'), 'manage_options', __FILE__, 'incentivibe_main_page', '', 90);
+	add_action('admin_init', 'incentivibe_register_settings');
+}
+
+function incentivibe_register_settings() {
+	register_setting( 'incentivibe_main', 'incentivibe_header_code' );
+}
+
+function incentivibe_main_page() {
+	?>
+<style>
+	p {
+		font-family: "Trebuchet MS", Helvetica, sans-serif;
+		font-size: 16px;
+		line-height: 29px;
+		width: 1000px;
+		
+	}
+	h3 {
+		font-size: 1.6em;
+	}
+</style>
+<div class="wrap">
+	<?php screen_icon('options-general'); ?>
+	<h2><?php _e('Incentivibe'); ?></h2>
+
+	<p><?php _e('
+		Incentivibe is the first and only company in the world, which specializes in shared giveaways for small businesses. With Incentivibe\'s Shared Giveaways, small businesses become co-sponsors of a great prize by sharing a fraction (e.g. $19.99) of the prize cost with other small businesses (free trial available), and offer their visitors a legitimate chance to win that prize.
+	'); ?></p>
+	
+	<h3><?php _e('How to Install?'); ?></h3>
+	
+	<p><?php _e('
+		To install incentivibe, please click <a href="http://www.incentivibe.com/users/sign_up?provider=wordpress" target="_blank">here</a> to register and generate a code. Paste that code over here and thats it! you are done!.
+	'); ?></p>
+	<form method="post" action="options.php">
+		<?php settings_fields( 'incentivibe_main' ); ?>
+		<?php /*do_settings( 'inscr_main' );*/ ?>
+		<textarea name="incentivibe_header_code" cols="100" rows="10"><?php echo get_option('incentivibe_header_code'); ?></textarea>
+		<?php submit_button(); ?>
+	</form>
+
+	<p><?php _e('Some text here'); ?></p>
+</div>
+<?php
+}
+
+function incentivibe_wp_head() {
+	echo get_option('incentivibe_header_code')."\n";
+}
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Incentivibe ===
+Plugin Name: Incentivibe
+Plugin URI: http://www.incentivibe.com
+Description: Increase email leads and social media exposure by up to 400%!
+Author: Incentivibe
+Author URI: http://www.incentivibe.com
+Tags: incentivibe giveaway promotion sweepstakes
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: 1.0
+
+== Description ==
+Incentivibe is the first and only company in the world, which specializes in shared giveaways for small businesses. With Incentivibe's Shared Giveaways, small businesses become co-sponsors of a great prize by sharing a fraction (e.g. $19.99) of the prize cost with other small businesses (free trial available), and offer their visitors a legitimate chance to win that prize.
+
+Installing Incentivibe will take only few seconds, install this plugin and then click <a href="http://www.incentivibe.com/users/sign_up?provider=wordpress" target="_blank">here</a> to register and generate a code. Paste the generated code in the plugin settings and thats it! You are done!.
+
+== Installation ==
+
+In three quick and easy steps you can start virally growing your email list and social media presence.
+
+e.g.
+
+1. Upload the entire incentivibe folder to the /wp-content/plugins/ directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Click <a href="http://www.incentivibe.com/users/sign_up?provider=wordpress" target="_blank">here</a> to generate the Incentivibe code to be placed in plugin.
+
+
